56d6b919740cc8439aa96bbcd1f00b25ea86b25a
Brandon Artner <brandon.artner739@myci.csuci.edu>
2018-03-06 17:21:18 -0800

    Updated schema

---
+++ TheLaw/db/migrate/20180307002256_drop_articles_table.rb
@@ -0,0 +1,9 @@
+class DropArticlesTable < ActiveRecord::Migration[5.1]
+  def up
+    drop_table :articles
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- TheLaw/db/schema.rb
+++ TheLaw/db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219004731) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180307002256) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
