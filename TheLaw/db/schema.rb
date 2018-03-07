55e672a69d246e068c956efbe20785870d7c3b77
Brandon Artner <brandon.artner739@myci.csuci.edu>
2018-03-06 16:17:29 -0800

    Updated some names

--- TheLaw/db/schema.rb
+++ TheLaw/db/schema.rb
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20180219004731) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id"
+    t.integer "document_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["document_id"], name: "index_comments_on_document_id"
   end
 
   create_table "documents", force: :cascade do |t|
